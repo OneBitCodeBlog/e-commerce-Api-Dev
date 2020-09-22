e39e4ebda79b827b037abac28624421ab16ec827
Daniel Moreto <dfmoreto@gmail.com>
2020-09-22 00:53:25 -0300

    Create model User for Devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
