8d89a99775b07fe03fa791d1f369b722d4bcb142
Daniel Moreto <dfmoreto@gmail.com>
2020-09-27 21:03:10 -0300

    Validate Coupon to only have future due dates

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,7 +4,7 @@ class Coupon < ApplicationRecord
   validates :status, presence: true
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
   validates :max_use, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :due_date, presence: true
+  validates :due_date, presence: true, future_date: true
 
   enum status: %i(active inactive)
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Coupon, type: :model do
   it { is_expected.to validate_presence_of :max_use }
   it { is_expected.to validate_numericality_of(:max_use).only_integer.is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of :due_date }
+
+  it "can't have past due_date" do
+    subject.due_date = 1.day.ago
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+
+  it "is invalid with current due_date" do
+    subject.due_date = Time.zone.now
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+
+  it "is valid with future date" do
+    subject.due_date = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.keys).to_not include :due_date
+  end
 end
