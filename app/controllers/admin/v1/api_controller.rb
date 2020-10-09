28c8ff333355553dcaf193559a721c12062d3aca
Daniel Moreto <dfmoreto@gmail.com>
2020-10-09 17:45:42 -0300

    Update Admin V1 Api controller Controller updated to start handling non-:admin accesses and using SimpleErrorRenderable concern

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,12 +1,22 @@
 module Admin::V1
   class ApiController < ApplicationController
+    class ForbiddenAccess < StandardError; end
+
     include Authenticatable
 
-    def render_error(message: nil, fields: nil, status: :unprocessable_entity)
-      errors = {}
-      errors['fields'] = fields if fields.present?
-      errors['message'] = message if message.present?
-      render json: { errors: errors }, status: status
+    include SimpleErrorRenderable
+    self.simple_error_partial = "shared/simple_error"
+
+    rescue_from ForbiddenAccess do 
+      render_error(message: "Forbidden access", status: :forbidden)
+    end
+
+    before_action :restrict_access_for_admin!
+
+    private
+
+    def restrict_access_for_admin!
+      raise ForbiddenAccess unless current_user.admin?
     end
   end
 end
