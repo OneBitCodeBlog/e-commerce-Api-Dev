03b74a8dee267fec6b9e5bbeb81f49f2bb8c2fe1
Daniel Moreto <dfmoreto@gmail.com>
2020-10-08 21:53:16 -0300

    Update model Category Model updated to add a :destroy dependency with :product_categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 
-  it { is_expected.to have_many :product_categories }
+  it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:products).through(:product_categories) }
 end
