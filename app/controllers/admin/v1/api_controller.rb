04a7dacda432b0af5ca24df8a9a745fe56cb5c03
Daniel Moreto <dfmoreto@gmail.com>
2020-10-09 16:54:23 -0300

    Create method to render error on Api controller This method was created to make a standard on error rendering on JSON

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,5 +1,12 @@
 module Admin::V1
   class ApiController < ApplicationController
     include Authenticatable
+
+    def render_error(message: nil, fields: nil, status: :unprocessable_entity)
+      errors = {}
+      errors['fields'] = fields if fields.present?
+      errors['message'] = message if message.present?
+      render json: { errors: errors }, status: status
+    end
   end
 end
