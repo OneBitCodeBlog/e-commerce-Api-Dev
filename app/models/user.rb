3d6602d778c84f573d46884002c7d07f9adbe8cb
Daniel Moreto <dfmoreto@gmail.com>
2020-10-10 19:58:21 -0300

    Update User model It was updated to has default profile as :client and fixed to not be confirmable as it was before

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  include DeviseTokenAuth::Concerns::User
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  include DeviseTokenAuth::Concerns::User
 
   validates :name, presence: true
   validates :profile, presence: true
--- db/migrate/20200920200639_devise_token_auth_create_users.rb
+++ db/migrate/20200920200639_devise_token_auth_create_users.rb
@@ -31,7 +31,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       ## User Info
       t.string :name
       t.string :email
-      t.integer :profile
+      t.integer :profile, default: 1
 
       ## Tokens
       t.json :tokens
