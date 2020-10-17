0860a635562b65515a489e8d4990ea536b382093
Daniel Moreto <dfmoreto@gmail.com>
2020-10-17 20:04:27 -0300

    Create endpoints for Users CRUD

---
+++ app/controllers/admin/v1/users_controller.rb
@@ -0,0 +1,44 @@
+module Admin::V1
+  class UsersController < ApiController
+    before_action :load_user, only: [:update, :destroy]
+
+    def index
+      @users = User.all
+    end
+
+    def create
+      @user = User.new
+      @user.attributes = user_params
+      save_user!
+    end
+
+    def update
+      @user.attributes = user_params
+      save_user!
+    end
+
+    def destroy
+      @user.destroy!
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
+
+    private
+
+    def load_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      return {} unless params.has_key?(:user)
+      params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :profile)
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+    rescue
+      render_error(fields: @user.errors.messages)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :coupons
       resources :products
       resources :system_requirements
+      resources :users
     end
   end
 
