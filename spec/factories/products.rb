0d05a48c2c936b9a3ea44bce1614366263b5f62e
Daniel Moreto <dfmoreto@gmail.com>
2020-10-18 17:45:33 -0300

    Add image on :product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name { Faker::Game.unique.title }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
     
     after :build do |product|
       product.productable = create(:game)
