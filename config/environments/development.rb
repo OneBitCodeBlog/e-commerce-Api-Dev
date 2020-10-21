7e9bc7b8f3c60bbd6585a34974b038b8d9e9a086
Daniel Moreto <dfmoreto@gmail.com>
2020-10-21 14:57:17 -0300

    Add SMTP settings on dev env for Mailcather

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,12 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025,
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
