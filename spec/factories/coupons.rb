c5a310bd17317c565ce39d0f48004cfd7787cdbc
Daniel Moreto <dfmoreto@gmail.com>
2020-10-21 18:30:00 -0300

    Fix Game and Coupon factory

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "My Coupon #{n}" }
     code { Faker::Commerce.promotion_code(digits: 2) }
     status { :active }
+    discount_value { 25 }
     max_use { 1 }
     due_date { 3.days.from_now }
   end
--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :game do
     mode { %i(pvp pve both).sample }
-    release_date { 5.days.ago }
+    release_date { '2020-06-01' }
     developer { Faker::Company.name }
     system_requirement
   end
