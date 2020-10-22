f6127d517745366b4a6819ce8ecaff4038c22fa1
Daniel Moreto <dfmoreto@gmail.com>
2020-10-22 13:50:10 -0300

    Change name generation of Product factory It should be changed to use sequence due to a limit of unique values for Faker::Game.name

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    name { Faker::Game.unique.title }
+    sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
     image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
