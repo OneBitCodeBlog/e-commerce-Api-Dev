efd2172271302f145278ab34229d91fba0b6209a
Daniel Moreto <dfmoreto@gmail.com>
2020-10-23 10:12:45 -0300

    Fix to ensure unique :code

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :coupon do
     sequence(:name) { |n| "My Coupon #{n}" }
-    code { Faker::Commerce.promotion_code(digits: 2) }
+    code { Faker::Commerce.unique.promotion_code(digits: 4) }
     status { :active }
     discount_value { 25 }
     max_use { 1 }
