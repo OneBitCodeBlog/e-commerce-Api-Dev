8328a67c68597e9b12ff49abf4d8645b93550511
Daniel <dfmoreto@gmail.com>
2020-10-25 17:41:00 -0300

    Aula 03 - Instalando Devise Token Auth

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth/v1/user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
