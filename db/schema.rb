bfc834719c2415151d3ab80cb9d0f08869ab3d60
Daniel <dfmoreto@gmail.com>
2020-10-27 23:11:00 -0300

    Aula 07 - Criação do primeiro Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_173829) do
+ActiveRecord::Schema.define(version: 2020_10_28_011712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
---
+++ spec/factories/categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :category do
+    sequence(:name) { |n| "Category #{n}" }
+  end
+end
