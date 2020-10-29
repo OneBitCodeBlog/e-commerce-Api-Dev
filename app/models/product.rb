c21cacbf68102b843f121e47bdcd65ec53c3b3cb
Daniel <dfmoreto@gmail.com>
2020-10-29 20:05:59 -0300

    Aula 17 - Adição de imagem para Produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,10 @@ class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
+  has_one_attached :image
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :image, presence: true
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
-
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
+    
     after :build do |product|
       product.productable = create(:game)
     end
