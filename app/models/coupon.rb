c60343cbbc68072a65d297bdfa9ffb6af30075e7
Daniel <dfmoreto@gmail.com>
2020-10-29 18:13:47 -0300

    Aula 15 - Utilização do Custom Validator no model Coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,7 +3,7 @@ class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
   validates :status, presence: true
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
-  validates :due_date, presence: true
+  validates :due_date, presence: true, future_date: true
 
   enum status: { active:1,  inactive: 2 }
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Coupon, type: :model do
   it { is_expected.to validate_presence_of :discount_value }
   it { is_expected.to validate_numericality_of(:discount_value).is_greater_than(0) }
   it { is_expected.to validate_presence_of :due_date }
+
+  it "can't have past due_date" do
+    subject.due_date = 1.day.ago
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+  
+  it "is invalid with current due_date" do
+    subject.due_date = Time.zone.now
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+  
+  it "is valid with future date" do
+    subject.due_date = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.keys).to_not include :due_date
+  end
 end
