b3c3744543ebc576d557382fb053c7fc5b87b17f
Daniel <dfmoreto@gmail.com>
2020-10-29 17:53:46 -0300

    Aula 14 - Criando uma Validação Customizada

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module ClassProduction
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
   end
 end
