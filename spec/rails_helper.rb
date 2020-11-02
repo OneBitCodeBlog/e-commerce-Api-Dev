e364c283f9632ea5b077212949a499206e0cf206
Daniel Moreto <dfmoreto@gmail.com>
2020-11-02 10:12:33 -0300

    Remove weird sort on spec support load

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
-Dir[Rails.root.join('spec', 'shared_examples', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'shared_examples', '**', '*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
