28fcfc547f5e2d1ac8b9b2a31bd2eb63cf159a6b
Daniel <dfmoreto@gmail.com>
2020-11-22 23:32:10 -0300

    Aula 30.2 - Status do Produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,7 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true
+  validates :status, presence: true
+
+  enum status: { available: 1, unavailable: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_213848) do
+ActiveRecord::Schema.define(version: 2020_11_23_022401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_213848) do
     t.bigint "productable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable_type_and_productable_id"
   end
 
