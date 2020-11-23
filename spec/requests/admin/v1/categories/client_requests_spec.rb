839ea3a1c8fd1e81f151758cb3cffbb66120ee6e
Daniel <dfmoreto@gmail.com>
2020-11-22 23:13:58 -0300

    Aula 30.1 - Rota Show para Categoria

--- spec/requests/admin/v1/categories/client_requests_spec.rb
+++ spec/requests/admin/v1/categories/client_requests_spec.rb
@@ -20,6 +20,15 @@ RSpec.describe "Admin V1 Categories as :client", type: :request do
     include_examples "forbidden access"
   end
 
+  context "GET /categories/:id" do
+    let(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+
+    before(:each) { get url, headers: auth_header(user) }
+
+    include_examples "forbidden access"
+  end
+
   context "PATCH /categories/:id" do
     let(:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{category.id}" }
