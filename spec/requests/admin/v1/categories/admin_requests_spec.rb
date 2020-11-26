d9ba0802720bb288609b001a12e303ef2d140a6a
Daniel Moreto <dfmoreto@gmail.com>
2020-11-26 08:52:47 -0300

    Update Category and Product GET requests specs They were updated to use new shared example for meta pagination

--- spec/requests/admin/v1/categories/admin_requests_spec.rb
+++ spec/requests/admin/v1/categories/admin_requests_spec.rb
@@ -23,6 +23,10 @@ RSpec.describe "Admin V1 Categories as :admin", type: :request do
         get url, headers: auth_header(user)
         expect(response).to have_http_status(:ok)
       end
+
+      it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total_pages: 1 } do
+        before { get url, headers: auth_header(user) }
+      end
     end
 
     context "with search[name] param" do
@@ -45,6 +49,10 @@ RSpec.describe "Admin V1 Categories as :admin", type: :request do
       it "returns success status" do
         get url, headers: auth_header(user), params: search_params
         expect(response).to have_http_status(:ok)
+      end
+
+      it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total_pages: 2 } do
+        before { get url, headers: auth_header(user), params: search_params }
       end
     end
 
@@ -69,6 +77,10 @@ RSpec.describe "Admin V1 Categories as :admin", type: :request do
         get url, headers: auth_header(user), params: pagination_params
         expect(response).to have_http_status(:ok)
       end
+
+      it_behaves_like 'pagination meta attributes', { page: 2, length: 5, total_pages: 2 } do
+        before { get url, headers: auth_header(user), params: pagination_params }
+      end
     end
 
     context "with order params" do
@@ -84,6 +96,10 @@ RSpec.describe "Admin V1 Categories as :admin", type: :request do
       it "returns success status" do
         get url, headers: auth_header(user), params: order_params
         expect(response).to have_http_status(:ok)
+      end
+
+      it_behaves_like 'pagination meta attributes', { page: 1, length: 10, total_pages: 1 } do
+        before { get url, headers: auth_header(user), params: order_params }
       end
     end
   end
