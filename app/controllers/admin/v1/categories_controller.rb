a79071876e6c4cc1d68f3a1660f1af69b0bdb965
Daniel Moreto <dfmoreto@gmail.com>
2020-11-26 08:46:59 -0300

    Change Products and Categories controller They were addapted to use new format of Modal Loading Service controller

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -3,7 +3,8 @@ module Admin::V1
     before_action :load_category, only: [:show, :update, :destroy]
 
     def index
-      @categories = load_categories
+      @loading_service = Admin::ModelLoadingService.new(Category.all, searchable_params)
+      @loading_service.call
     end
 
     def create
@@ -31,9 +32,8 @@ module Admin::V1
       @category = Category.find(params[:id])
     end
 
-    def load_categories
-      permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
-      Admin::ModelLoadingService.new(Category.all, permitted).call
+    def searchable_params
+      params.permit({ search: :name }, { order: {} }, :page, :length)
     end
 
     def category_params
