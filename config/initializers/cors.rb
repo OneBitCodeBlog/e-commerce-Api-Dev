de650d6615dfc3ca65630ce13cc18e26c1dbf14b
Daniel Moreto <dfmoreto@gmail.com>
2020-11-28 14:03:21 -0300

    Expose devise token auth headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete]
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete],
+                  expose: ['access-token', 'client', 'expiry', 'token-type', 'uid']
   end
 end
