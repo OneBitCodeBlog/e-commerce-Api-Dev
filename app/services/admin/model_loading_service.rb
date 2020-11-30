ed9e1a6c9128f4720ab15debe4c5f374df88ff1e
Daniel <dfmoreto@gmail.com>
2020-11-30 08:26:50 -0300

    Aula 36 - Refatoração: Paginação (Parte 01)

--- app/services/admin/model_loading_service.rb
+++ app/services/admin/model_loading_service.rb
@@ -1,15 +1,27 @@
 module Admin  
   class ModelLoadingService
+    attr_reader :records, :pagination
+
     def initialize(searchable_model, params = {})
       @searchable_model = searchable_model
-      @params = params
-      @params ||= {}
+      @params = params || {}
+      @records = []
+      @pagination = { page: @params[:page].to_i, length: @params[:length].to_i }
     end
 
     def call
-      @searchable_model.search_by_name(@params.dig(:search, :name))
-                       .order(@params[:order].to_h)
-                       .paginate(@params[:page].to_i, @params[:length].to_i)
+      fix_pagination_values
+      filtered = @searchable_model.search_by_name(@params.dig(:search, :name))
+      @records = filtered.order(@params[:order].to_h).paginate(@pagination[:page], @pagination[:length])
+      total_pages = (filtered.count / @pagination[:length].to_f).ceil
+      @pagination.merge!(total: filtered.count, total_pages: total_pages)
+    end
+
+    private
+
+    def fix_pagination_values
+      @pagination[:page] = @searchable_model.model::DEFAULT_PAGE if @pagination[:page] <= 0
+      @pagination[:length] = @searchable_model.model::MAX_PER_PAGE if @pagination[:length] <= 0
     end
   end
 end
