f8642061d76403a3bfc71b477df0080b17da2f1a
Daniel <dfmoreto@gmail.com>
2021-01-06 23:06:27 -0300

    Remove concern for name search

--- app/models/concerns/name_searchable.rb
+++
@@ -1,9 +0,0 @@
-module NameSearchable
-  extend ActiveSupport::Concern
-
-  included do
-    scope :search_by_name, -> (value) do
-      self.where("name ILIKE ?", "%#{value}%")
-    end
-  end
-end
