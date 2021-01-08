507598d4e25ca39d1c62c04c6c61e55ad3e5f6e4
Daniel <dfmoreto@gmail.com>
2021-01-07 22:35:52 -0300

    Update _product partial It was updated to add :productable_it in JSON rendering

--- spec/requests/admin/v1/products/admin_requests_spec.rb
+++ spec/requests/admin/v1/products/admin_requests_spec.rb
@@ -446,6 +446,7 @@ def build_game_product_json(product)
   json = product.as_json(only: %i(id name description price status))
   json['image_url'] = rails_blob_url(product.image)
   json['productable'] = product.productable_type.underscore
+  json['productable_id'] = product.productable_id
   json['categories'] = product.categories.as_json
   json.merge! product.productable.as_json(only: %i(mode release_date developer))
   json['system_requirement'] = product.productable.system_requirement.as_json
