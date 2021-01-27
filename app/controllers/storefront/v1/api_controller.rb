78b02e6210fd22cb6b24bca001d1e9dec894d66e
Daniel Moreto <dfmoreto@gmail.com>
2021-01-26 23:29:38 -0300

    Update Storefront ApiController Included SimpleErrorRenderable concern

--- app/controllers/storefront/v1/api_controller.rb
+++ app/controllers/storefront/v1/api_controller.rb
@@ -1,5 +1,8 @@
 module Storefront::V1
   class ApiController < ApplicationController
     include Authenticatable
+
+    include SimpleErrorRenderable
+    self.simple_error_partial = "shared/simple_error"
   end
 end
