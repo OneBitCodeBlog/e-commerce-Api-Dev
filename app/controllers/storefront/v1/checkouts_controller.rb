b1826a1d9425979508f2133da1513b7c89a46701
Daniel Moreto <dfmoreto@gmail.com>
2021-02-13 22:42:29 -0300

    Create Checkouts Controller

---
+++ app/controllers/storefront/v1/checkouts_controller.rb
@@ -0,0 +1,23 @@
+module Storefront::V1
+  class CheckoutsController < ApiController
+    def create
+      run_service
+    rescue Storefront::CheckoutProcessorService::InvalidParamsError
+      render_error(fields: @service.errors)
+    end
+
+    private
+
+    def run_service
+      @service = Storefront::CheckoutProcessorService.new(checkout_params)
+      @service.call
+      render :show
+    end
+
+    def checkout_params
+      params.require(:checkout).permit(:subtotal, :total_amount, :payment_type, :installments, :coupon,
+                                       items: [:quantity, :payed_price, :product_id])
+                               .reverse_merge(user_id: current_user.id, installments: 1)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       get "home" => "home#index"
       resources :products, only: [:index, :show]
       resources :categories, only: :index
+      resources :checkouts, only: :create
       post "/coupons/:coupon_code/validations", to: "coupon_validations#create"
       resources :wish_items, only: [:index, :create, :destroy]
     end
