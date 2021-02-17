1662f1144824b6b8ae9dbf9ff2df8e722368582f
Daniel Moreto <dfmoreto@gmail.com>
2021-02-17 09:03:46 -0300

    Update Storefront Checkouts Controller This controller was updated to permit :card_hash e :address

--- app/controllers/storefront/v1/checkouts_controller.rb
+++ app/controllers/storefront/v1/checkouts_controller.rb
@@ -15,8 +15,9 @@ module Storefront::V1
     end
 
     def checkout_params
-      params.require(:checkout).permit(:subtotal, :total_amount, :payment_type, :installments, :coupon,
-                                       items: [:quantity, :payed_price, :product_id])
+      params.require(:checkout).permit(:subtotal, :total_amount, :payment_type, :installments, :coupon, :card_hash,
+                                       items: [:quantity, :payed_price, :product_id],
+                                       address: [:street, :number, :city, :state, :post_code])
                                .reverse_merge(user_id: current_user.id, installments: 1)
     end
   end
