31c58a314394d31d61ddead5ec4d2228ca083dba
Daniel Moreto <dfmoreto@gmail.com>
2021-02-28 12:54:40 -0300

    Update dev and test env config It was updated to add a contant JUNO_BASE_URL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  JUNO_BASE_URL = "https://sandbox.boletobancario.com"
 end
