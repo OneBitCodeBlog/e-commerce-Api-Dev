e0f10a65456164fd10d665098ac1231225fafb21
Daniel Moreto <dfmoreto@gmail.com>
2021-03-04 23:41:31 -0300

    Fix Juno Auth spec to right mock HTTParty

--- spec/lib/juno/auth_spec.rb
+++ spec/lib/juno/auth_spec.rb
@@ -4,25 +4,48 @@ require_relative "../../../lib/juno/auth"
 describe Juno::Auth do
   let(:auth_class) { Juno::Auth.clone }
 
-  it "has only one instance" do
-    object_ids = 0.upto(4).collect do 
-      auth_class.singleton
+  context "when call #singleton" do
+    let(:response) do
+      double(
+        parsed_response: { 'access_token' => SecureRandom.hex, 'expires_in' => 1.day.from_now.to_i },
+        code: 200
+      )
     end
-    object_ids.uniq!
-    expect(object_ids.size).to eq 1 
+
+    it "returns only one instance" do
+      allow(auth_class).to receive(:post).and_return(response)
+      object_ids = 0.upto(4).collect do 
+        auth_class.singleton
+      end
+      object_ids.uniq!
+      expect(object_ids.size).to eq 1 
+    end
+
+    it "call Juno API only once" do
+      allow(auth_class).to receive(:post).and_return(response).once
+      object_ids = 0.upto(4).collect do 
+        auth_class.singleton
+      end
+    end
   end
 
   context "when call #access_token" do
     let(:first_response) do
-      double(body: { 'access_token' => SecureRandom.hex, 'expires_in' => 1.day.from_now.to_i })
+      double(
+        parsed_response: { 'access_token' => SecureRandom.hex, 'expires_in' => 1.day },
+        code: 200
+      )
     end
 
     let(:second_response) do
-      double(body: { 'access_token' => SecureRandom.hex, 'expires_in' => 1.day.from_now.to_i })
+      double(
+        parsed_response: { 'access_token' => SecureRandom.hex, 'expires_in' => 1.day },
+        code: 200
+      )
     end
 
     before(:each) do
-      allow(auth_class).to receive(:get).and_return(first_response, second_response)
+      allow(auth_class).to receive(:post).and_return(first_response, second_response)
     end
 
     it "returns same access token before expiration" do
@@ -43,7 +66,7 @@ describe Juno::Auth do
       first_auth = auth_class.singleton
       seconds_to_travel = first_auth.expires_in * 0.91
       travel seconds_to_travel.seconds do
-        second_auth = Juno::Auth.singleton
+        second_auth = auth_class.singleton
         expect(first_auth.access_token).to_not eq second_auth.access_token
       end
     end
