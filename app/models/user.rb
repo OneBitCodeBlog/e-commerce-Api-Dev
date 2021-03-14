46a5cef72f00c5ddcba53d601966059965ff4f7a
Daniel Moreto <dfmoreto@gmail.com>
2021-03-13 21:10:25 -0300

    Update model User Add a has_many association with Order

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   has_many :wish_items
+  has_many :orders
 
   validates :name, presence: true
   validates :profile, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_many :wish_items }
+  it { is_expected.to have_many :orders }
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:profile) }
