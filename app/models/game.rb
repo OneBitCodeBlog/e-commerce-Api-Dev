ecf1759a68306396f997d263ca8a756e4ee59de5
Daniel Moreto <dfmoreto@gmail.com>
2021-03-15 08:02:27 -0300

    Create a method #ship on Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,4 +10,8 @@ class Game < ApplicationRecord
   validates :developer, presence: true
 
   enum mode: { pvp: 1, pve: 2, both: 3 }
+
+  def ship!(line_item)
+    Admin::AlocateLicenseJob.perform_later(line_item)
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Game, type: :model do
   it { is_expected.to have_many :licenses }
 
   it_has_behavior_of "like searchable concern", :game, :developer
+
+  it "#ship! must schedule job AlocateLicenseJob sending Line Item" do
+    subject.product = create(:product)
+    line_item = create(:line_item, product: subject.product)
+    expect do
+      subject.ship!(line_item)
+    end.to have_enqueued_job(Admin::AlocateLicenseJob).with(line_item)
+  end
 end
