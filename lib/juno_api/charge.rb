9483cb18ae132e12663d906c547bfaef5cb91adc
Daniel Moreto <dfmoreto@gmail.com>
2021-04-25 21:57:28 -0300

    Aula 68 - Módulo de Cobrança na Juno

---
+++ lib/juno_api/charge.rb
@@ -0,0 +1,63 @@
+require_relative "./auth"
+require_relative "./request_error"
+
+module JunoApi
+  class Charge
+    include HTTParty
+
+    PAYMENT_TYPE = { 'billet' => "BOLETO", 'credit_card' =>"CREDIT_CARD" }
+    CHARGE_KEYS_TO_KEEP = %i[id code installment_link amount status]  
+
+    base_uri "#{JUNO_RESOURCE_URL}/charges"
+
+    headers 'Content-Type' => 'application/json' 
+    headers 'X-Api-Version' => '2'
+    headers 'X-Resource-Token' => Rails.application.credentials.juno[:private_token]
+
+    def initialize
+      @auth = Auth.singleton
+    end
+
+    def create!(order)
+      auth_header = { 'Authorization' => "Bearer #{auth.access_token}" }
+      body = prepare_create_body(order)
+      response = self.class.post("/", headers: auth_header, body: body.to_json)
+      raise_error(response) if response.code != 200
+      organize_response(response)
+    end
+    
+    private
+    
+    attr_reader :auth, :auth_header
+    
+    def prepare_create_body(order)
+      { 
+        charge: build_charge(order),
+        billing: { name: order.user.name, document: order.document, email: order.user.email }
+      }
+    end
+
+    def raise_error(response)
+      details = response.parsed_response['details'].map { |detail| detail.transform_keys(&:underscore) }
+      raise RequestError.new("Invalid request sent to Juno", details)
+    rescue NoMethodError => e
+      raise RequestError.new("Invalid request sent to Juno")
+    end
+
+    def organize_response(response)
+      response.parsed_response['_embedded']['charges'].map do |charge|
+        charge.deep_transform_keys! { |key| key.underscore.to_sym }
+        charge.keep_if { |key, _| CHARGE_KEYS_TO_KEEP.include?(key) }
+      end
+    end
+
+    def build_charge(order)
+      { 
+        description: "Order ##{order.id}", amount: (order.total_amount / order.installments).floor(2), 
+        dueDate: order.due_date.strftime("%Y-%m-%d"), installments: order.installments, 
+        discountAmount: (order.coupon&.discount_value).to_f, paymentTypes: [PAYMENT_TYPE[order.payment_type]]
+      }
+    end
+    
+  end
+end
---
+++ spec/libs/juno_api/charge_spec.rb
@@ -0,0 +1,63 @@
+require "rails_helper"
+require_relative "../../../lib/juno_api/charge"
+
+describe JunoApi::Charge do
+  let!(:order) { create(:order) }
+
+  describe "#create" do
+    before(:each) do
+      singleton = double(access_token: SecureRandom.hex)
+      allow(JunoApi::Auth).to receive(:singleton).and_return(singleton)
+    end
+
+    context "with invalid params" do
+      it "should raise an error" do
+        error = { details: [{ message: "Some error", errorCode: "10000" }] }.to_json
+        error_response = double(code: 400, body: error, parsed_response: JSON.parse(error))
+        allow(JunoApi::Charge).to receive(:post).and_return(error_response)
+        expect do
+          described_class.new.create!(order)
+        end.to raise_error(JunoApi::RequestError)
+      end
+    end
+
+    context "with valid params" do
+      let(:return_from_api) do
+        installment_to_pay = (order.total_amount / order.installments).floor(2)
+        charges = 0.upto(order.installments - 1).map do |num|
+          { 
+            id: "000#{num}", code: num, dueDate: (order.due_date + num.months).strftime("%Y-%m-%d"),
+            reference: "", amount: installment_to_pay, checkoutUrl: Faker::Internet.url(host: 'checkout.juno.com'),
+            status: "ACTIVE", _links: { self: { href: Faker::Internet.url(host: 'checkout.juno.com') } } 
+          }
+        end
+        { _embedded: { charges: charges } }.to_json
+      end
+
+      before(:each) do
+        api_response = double(code: 200, body: return_from_api, parsed_response: JSON.parse(return_from_api))
+        allow(JunoApi::Charge).to receive(:post).and_return(api_response)
+      end
+
+      it "returns same quantity of charges as installments" do
+        charges = described_class.new.create!(order)
+        expect(charges.count).to eq order.installments
+      end
+
+      it "return all charges with same installment amout" do
+        charges = described_class.new.create!(order)
+        installment_amount = charges.map { |charge| charge[:amount] }.uniq
+        expect(installment_amount.size).to eq 1
+      end
+
+
+      it "return right amount on each installment" do
+        installment_for_payment = (order.total_amount / order.installments.to_f).floor(2).to_f
+        charges = described_class.new.create!(order)
+        charges.each do |charge| 
+          expect(charge[:amount].to_f).to eq installment_for_payment
+        end
+      end
+    end
+  end
+end
