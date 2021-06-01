243746759376d62384ec5dc23005eb498152a450
Daniel Moreto <dfmoreto@gmail.com>
2021-06-01 18:24:57 -0300

    Aula 99 - Protegendo Endpoint de Confirmação de Pagamento

--- app/controllers/juno/v1/payment_confirmations_controller.rb
+++ app/controllers/juno/v1/payment_confirmations_controller.rb
@@ -1,5 +1,7 @@
 module Juno::V1
   class PaymentConfirmationsController < ApplicationController
+    include StaticTokenAuthenticatable
+    
     def create
       if params.has_key?(:chargeCode)
         Juno::Charge.find_by(code: params[:chargeCode])&.order&.update(status: :payment_accepted)
