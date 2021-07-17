2c6fe94eaaaac00ac9bd89ef5661a24f0939e333
Daniel Moreto <dfmoreto@gmail.com>
2021-07-17 13:22:03 -0300

    Update products factory It was updated to create products with prices between 5.00 and 30.00

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
-    price { Faker::Commerce.price(range: 100.0..400.0) }
+    price { Faker::Commerce.price(range: 5.0..30.0) }
     image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
     status { :available }
     featured { true }
